872dc0dc8343b71c32f83e7e2148804b8599acda
Guga <g.juma.tm@gmail.com>
2022-04-19 00:44:12 +0800

    testing

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,16 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Header from '../components/header';
 import LayOut from '../components/layout';
 
 export default function Home() {
   return (
-    <LayOut>
-      <header>
-        <div className="logo">Logo</div>
-        <div className="search">
-          <input type="text" placeholder="Search for food, coffee, etc..." />
-        </div>
-        <div className="categories">
-          <ul role="list">
-            <li>Hot Dishes</li>
-            <li>Cold Dishes</li>
-            <li>Soup</li>
-            <li>Grill</li>
-            <li>Appetizer</li>
-            <li>Dessert</li>
-          </ul>
-        </div>
-      </header>
-    </LayOut>
+    <>
+      <Header pageTitle="Welcome to Guga's" />
+      <p>
+        Here will be some carousel, bonus cards, new products, discounts etc
+        information
+      </p>
+    </>
   );
 }
